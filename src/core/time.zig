14aeba8e61ba5cc55a706521b3fe1824b68d84ce
joshdotmartin1281 <josh.martin1281@gmail.com>
2026-03-25 00:17:34 -0400

    refactor(Assets):
    Added assets directory at the top to be able to have a load path for my
    obj files. Also flattened clock.zig and time.zig and just have a
    time.zig now.

--- src/core/time.zig
+++ src/core/time.zig
@@ -1,10 +1,33 @@
-//- src/util/time.zig
 const std = @import("std");
-const Clock = @import("./clock.zig").Clock;
-const ClockConfig = @import("./clock.zig").ClockConfig;
 const c = @import("../platform/window.zig").c;
 
 pub const TimerConfig = struct { target_ups: f32 = 60.0, max_fps: u32 = 0, clock: ClockConfig = .{} };
+
+pub const ClockConfig = struct { time_scale: f32 = 1.0, max_delta: f32 = 0.1,};
+
+pub const Clock = struct {
+    timer: std.time.Timer,
+    last_frame: u64,
+
+    pub fn init() !Clock {
+        var t = try std.time.Timer.start();
+        return .{
+            .timer = t,
+            .last_frame = t.read(),
+        };
+    }
+
+    pub fn calculateDelta(self: *Clock, config: ClockConfig) f32 {
+        const now = self.timer.read();
+        const elapsed = now - self.last_frame;
+        self.last_frame = now;
+
+        var dt = @as(f32, @floatFromInt(elapsed)) / @as(f32, @floatFromInt(std.time.ns_per_s));
+        if (dt > config.max_delta) dt = config.max_delta;
+
+        return dt * config.time_scale;
+    }
+};
 
 pub const Timer = struct {
     clock: Clock,
